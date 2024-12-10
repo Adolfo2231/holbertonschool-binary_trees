c321ab95d8ec28760e030c0eea12d7a88d9941f2
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-12-10 12:02:11 -0400

    fix return and libraries

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include "binary_trees.h"
 
 /**
  * binary_tree_is_root - Checks if a node is the root of a binary tree
@@ -15,5 +14,4 @@ int binary_tree_is_root(const binary_tree_t *node)
 
 	if (node->parent == NULL)
 		return (1);
-	return (0);
 }
